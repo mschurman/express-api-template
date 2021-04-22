4ebe011fa628ef83acf298eee5185574801923d5
mschurman <schurman_mike@yahoo.com>
2021-04-21 21:51:48 -0400

    Configured access to environment settings via dotenv

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,9 +1,10 @@
 import express from 'express';
+import { testEnvironmentVariable } from '../settings';
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) =>
-  res.status(200).json({ message: 'Welcome to Express API template' })
+indexRouter.get('/', (req, res) => 
+  res.status(200).json({ message: 'Welcome to  Express API template. ' + testEnvironmentVariable })
 );
 
 export default indexRouter;
